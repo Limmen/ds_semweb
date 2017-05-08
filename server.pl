7abca83ffa25c09afdb187b720530dd08911264e
Limmen <kimham@kth.se>
2017-05-08 08:20:17 +0200

    update server

--- server.pl
+++ server.pl
@@ -1,3 +1,15 @@
+%% server.pl
+%% swi-prolog
+%% compile: ['server.pl']
+%%
+%% Http+Pengine server hosting static files and pengine safe-predicates
+%%
+%% Author Kim Hammar limmen@github.com <kimham@kth.se>
+
+%%%===================================================================
+%%% Facts
+%%%===================================================================
+
 :- module(pengine_server,
           [
            server/1,
@@ -18,7 +30,7 @@
 :- cors_enable.
 :- multifile sandbox:safe_primitive/1.
 
-
+%% Safe predicates accessible from remote client
 sandbox:safe_primitive(rdf_db:rdf(_,_,_)).
 sandbox:safe_primitive(ds_reasoner:reducible(_,_)).
 sandbox:safe_primitive(ds_reasoner:solvable(_,_)).
@@ -27,8 +39,18 @@ sandbox:safe_primitive(ds_reasoner:equivalent_model(_,_)).
 sandbox:safe_primitive(ds_reasoner:algorithm_can_solve(_,_)).
 sandbox:safe_primitive(ds_reasoner:algorithm_stronger_than(_,_)).
 
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%% Start http/pengine server on Port
+%% server(+).
+%% server(Port).
 server(Port):-
     http_server(http_dispatch, [port(Port)]).
 
+%% Stop http/pengine server on Port
+%% stop(+).
+%% stop(Port).
 stop(Port):-
     http_stop_server(Port, []).
